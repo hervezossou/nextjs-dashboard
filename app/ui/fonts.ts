b56d8ec473994f0ac98c3eb872b1761debd881a3
Hervé Zossou <hervezossou8@gmail.com>
2025-04-29 13:16:52 +0100

    feat: added dashboard layout and nested route pages

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { DM_Sans } from "next/font/google";
+import { Lusitana } from "next/font/google";
+
+export const dmSans = DM_Sans({ subsets: ["latin"] });
+export const lusitana = Lusitana({ 
+    subsets: ["latin"], 
+    weight: ["400", "700"], 
+});
