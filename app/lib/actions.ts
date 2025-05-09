2a80bd9cef205b63c7fbd3d0863324f3227bcdd5
Hervé Zossou <hervezossou8@gmail.com>
2025-05-09 19:33:10 +0100

    added server side form fields validation in edit-form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -73,13 +73,22 @@ export async function createInvoice(prevState: State, formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData, ) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice( id: string, prevState: State, formData: FormData, ) {
+    const validatedFields = UpdateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
 
+    if(!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: "Missing Fields. Failed to Update Invoice.",
+        };
+    }
+
+    // Prepare data for insertion into the database
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
 
     try { 
@@ -89,7 +98,7 @@ export async function updateInvoice(id: string, formData: FormData, ) {
             WHERE id = ${id}
         `;
     } catch(error) {
-        console.log("Failed to update the invoice", error)
+        return { message : "Database Error: Failed to Update Invoice."}
     }
 
     revalidatePath("/dashboard/invoices")
